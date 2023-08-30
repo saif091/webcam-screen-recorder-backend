3d2384bc79cec7cc2ec076988d6b86fb05f54494
saif091 <saifmalik7979@gmail.com>
2023-08-30 14:43:46 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose';
+import {userRouter} from './routes/users.js'
+
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth",userRouter);
+
+mongoose.connect("mongodb+srv://saifmalik:authmern@cluster0.ug33avx.mongodb.net/?retryWrites=true&w=majority")
+
+
+
+
+
+app.listen(4001,()=>console.log("server is running on port 4001"))
