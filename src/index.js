3edb487159127dd8624bcad64441cde8373eeab4
saif091 <92885692+saif091@users.noreply.github.com>
2023-08-30 15:55:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 
 app.use(express.json());
-app.use(cors({origin:["http:localhost:3000","https://mern-recorder.onrender.com/"]}));
+app.use(cors());
 
 app.use("/auth",userRouter);
 
